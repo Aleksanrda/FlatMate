5ee2479f89b72d7451a5b84a4331c29d6ae024b1
Aleksanrda <kryvkoalexandra05@gmail.com>
2023-03-18 15:12:41 +0000

    Added basic calculator

--- TenantMate.Education/Program.cs
+++ TenantMate.Education/Program.cs
@@ -1,3 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿string value;
 
-Console.WriteLine("Education x2!");
+do
+{
+    int res;
+
+    Console.WriteLine("Enter first number:");
+
+    int num1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Enter second number:");
+
+    int num2 = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Enter symbol(+, -, *, /):");
+
+    var symbol = Console.ReadLine();
+
+    switch (symbol)
+    {
+        case "+":
+            res = num1 + num2;
+            Console.WriteLine("Addition:" + res);
+            break;
+        case "-":
+            res = num1 - num2;
+            Console.WriteLine("Subtraction:" + res);
+            break;
+        case "*":
+            res = num1 * num2;
+            Console.WriteLine("Multiplication:" + res);
+            break;
+        case "/":
+            res = num1 / num2;
+            Console.WriteLine("Division:" + res);
+            break;
+        default:
+            Console.WriteLine("Wrong input");
+            break;
+    }
+
+    Console.ReadLine();
+
+    Console.Write("Do you want to continue(y/n):");
+    value = Console.ReadLine();
+} while (value is "y" or "Y");
+
